ff796c15b04df23f9cf8a5127e8bd56f40978dd1
J. Cohen <hostelmarketing@gmail.com>
2020-03-18 15:15:31 -0700

    Add cat emails

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -4,8 +4,12 @@ import "./card.styles.css";
 export default function Card({ cat }) {
     return (
         <div className="card-container">
-            <img src={`https://robohash.org/${cat.id}?set=set4&size=180x180`} alt={cat.name} />
+            <img
+                src={`https://robohash.org/${cat.id}?set=set4&size=180x180`}
+                alt={cat.name}
+            />
             <h1>{cat.name}</h1>
+            <p>{cat.email}</p>
         </div>
     );
 }
